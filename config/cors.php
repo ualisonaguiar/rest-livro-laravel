bcb859d3e4ae989e445b843c54798a20021be46e
Ualison Aguiar <ualison.aguiar@gmail.com>
2025-09-07 16:48:42 -0300

    ajustando o cors

--- config/cors.php
+++ config/cors.php
@@ -2,11 +2,4 @@
 
 return [
     'paths' => ['api/*'],
-    'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:5173'], // porta do frontend
-    'allowed_origins_patterns' => [],
-    'allowed_headers' => ['*'], // já permite todos os headers
-    'exposed_headers' => [], // opcional
-    'max_age' => 0,
-    'supports_credentials' => true, // se precisar enviar cookies
 ];
